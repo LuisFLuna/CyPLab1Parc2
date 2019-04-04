72510a84b93e45b85d91a21bde74fc14d70a6391
LuisFLuna <luigifleed2@hotmail.com>
2019-04-04 14:58:13 -0600

    greet feature

---
+++ src/app/greet/greet.spec.ts
@@ -0,0 +1,5 @@
+import { greet} from ’./ greet ’;
+
+describe (’greet’, ()=>{ // greet suit
+  it (’should include the name in the message ’, () => { expect(greet(’adsoft ’)).toContain(’adsoft ’);
+}) })
