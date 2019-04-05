33bb3d7106949eb5a8b244f40b92b0c20c639129
LuisFLuna <luigifleed2@hotmail.com>
2019-04-04 18:33:08 -0600

    All together implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'laboratorio1Parcial2CyP';
+  title = 'angular-integration-testing';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VoterComponent } from './voter/voter.component';
+import { TodosComponent } from './todos/todos.component';
+import { UsersComponent } from './users/users.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoterComponent,
+    TodosComponent,
+    UsersComponent,
+    UserDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
